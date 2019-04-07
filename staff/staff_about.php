04dec3a0d3b882a73820586fa67def7837efc63d
Siddharth2728 <brajesh.prasad28@gmail.com>
2019-04-07 13:46:48 +0530

    COMIT

---
+++ staff/staff_about.php
@@ -0,0 +1,209 @@
+<?php
+ require_once('../dbconnection.php');
+ require('staff_details.php');
+
+if(isset($_POST['submit'])){
+  echo "<pre>";
+  print_r($_POST);
+  die();
+}
+ ?>
+
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <!-- <meta http-equiv="refresh" content="2"> -->
+       <title>AboutUs</title>
+       <link rel="stylesheet" href="../owlcarousel/owl.carousel.min.css">
+       <link rel="stylesheet" href="../owlcarousel/owl.carousel.min.js">
+       <link rel="shortcut icon" type="images/png" href="../images/drop.png">
+       <meta name="viewport" content="width-device-width initial-scale=1">
+       <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+       <link rel="stylesheet" href="../css/styleabc.css">
+       <script src="../bootstrap/jquery.min.js"></script>
+
+       <script src="../bootstrap/bootstrap.min.js"></script>
+       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  </head>
+
+
+
+   <body>
+     <nav>
+       <ul>
+         <P style="color: gray; float: left; margin: 6px; font-weight: bolder; font-size: 20px; font-style: italic;">WELCOME, <?php echo $staff->name; ?></p>
+         <li><a href="https://www.facebook.com/bkumarprasad" class="facebook"><i class="fa fa-facebook" style="background:#3B5998; color: white; padding:10px;"></i></a> </li>
+         <li><a href="#" class="twitter"><i class="fa fa-twitter" style="background:  #55ACEE; color: white; padding:10px;"></i></a></li>
+         <li><a href="#" class="google"><i class="fa fa-google" style="background:  #dd4b39; color:white; padding:10px;"></i></a></li>
+         <li><a href="#" class="linkedin"><i class="fa fa-linkedin" style="background:#007bb5; color:white; padding:10px;"></i></a></li>
+         <li><a href="#" class="youtube"><i class="fa fa-youtube" style="background:#bb0000; color:white; padding:10px;"></i></a></li>
+       </ul>
+     </nav>
+
+     <!-- header image slider here -->
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+         <!-- Indicators -->
+         <ol class="carousel-indicators">
+           <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+           <li data-target="#myCarousel" data-slide-to="1"></li>
+           <li data-target="#myCarousel" data-slide-to="2"></li>
+           <li data-target="#myCarousel" data-slide-to="3"</li>
+           <li data-target="#myCarousel" data-slide-to="4"></li>
+           <li data-target="#myCarousel" data-slide-to="5"></li>
+           <li data-target="#myCarousel" data-slide-to="6"></li>
+           <li data-target="#myCarousel" data-slide-to="7"></li>
+           <li data-target="#myCarousel" data-slide-to="8"></li>
+           <li data-target="#myCarousel" data-slide-to="9"></li>
+           <li data-target="#myCarousel" data-slide-to="10"></li>
+           <li data-target="#myCarousel" data-slide-to="11"></li>
+           <li data-target="#myCarousel" data-slide-to="12"></li>
+           <li data-target="#myCarousel" data-slide-to="13"></li>
+         </ol>
+
+         <!-- Wrapper for slides -->
+         <div class="carousel-inner">
+           <div class="item active">
+             <img src="../images/blood-bank.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/Blood-Donor-Testing.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/benefits-of-blood-donation.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/1.jpg" style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/blood-screening-tests.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/donate-blood.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/Fullscreen-capture-2812014-30133-AM.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/m6840119_763x260-give-blood.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/DGZDGSDFG.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/1222719906.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/BANNER1.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/BANNER3.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images/blood-bank.jpg"  style="width:100%; height:321px;">
+           </div>
+
+           <div class="item">
+             <img src="../images\blood-donation.jpg" style="width:100%; height:321px;">
+           </div>
+         </div>
+
+         <!-- Left and right controls -->
+         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+           <span class="glyphicon glyphicon-chevron-left"></span>
+           <span class="sr-only">Previous</span>
+         </a>
+         <a class="right carousel-control" href="#myCarousel" data-slide="next">
+           <span class="glyphicon glyphicon-chevron-right"></span>
+           <span class="sr-only">Next</span>
+         </a>
+       </div>
+
+
+
+ <?php require("staff_header.php"); ?>
+ <div class="container">
+   <div class="row">
+   <div id="about-us">
+       <img src="../images/about-us.png" style="height: 220px; width: 406px; position: static; padding-bottom:16px; padding-top: 32px; margin: 22px 370px;"><br>
+   </div>
+
+   <div class="col-sm-9 pull-left" style="position:static;">
+     <p>My mother is an honest woman.</p>
+
+     <p>Similarly, when the first letter of a word is a vowel but is pronounced with a consonant sound, use a, as in the sample sentence below:
+
+     She is an United States senator.
+     She is a United States senator.
+
+     This holds true with acronyms and initialisms, too: an LCD display, a UK-based company, an HR department, a URL.
+     Article Before an Adjective
+
+
+
+     There are a few exceptions to the general rule of using a before words that start with consonants and an before words that begin with vowels. The first letter of the word honor, for example, is a consonant, but it’s unpronounced. In spite of its spelling, the word honor begins with a vowel sound. Therefore, we use an. Consider the example sentence below for an illustration of this concept.
+
+     My mother is a honest woman.
+     My mother is an honest woman.
+
+     Similarly, when the first letter of a word is a vowel but is pronounced with a consonant sound, use a, as in the sample sentence below:
+ Sophie’s party.
+ I heard an interesting story yesterday.</p>
+ </div><br>
+
+ <div class="col-sm-3 pull-right" style="position:static; height:345px; background:white; padding:-1px; margin-top: 16px; border: 1px solid rgb(135, 58, 45); border-radius:9px; overflow:hidden;">
+   <H3 style="font-family:san-serif;  text-align: center; background:orange; margin-top: 0px;">UPDATES</H3><BR>
+     <div class="ticker">
+       <div class="marquee">
+         <marquee style="height:300px; margin-top:-18px;" scrollamount="2" scrolldelay="2" direction="up" onmouseover="this.stop()" onmouseout="this.start()">
+         <ul id=update>
+           <li>this is my fierst newesfggggggggggggggggggggggggggggggg</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+           <li>this is my fierst newes</li>
+         </ul></marquee></div></div>
+
+ </div>
+</div></div><hr>
+
+ <!--footer starts here--->
+ <footer>
+   <div class="footer">
+       &copy2018 all right reserved
+   </div>
+ </footer>
+ <!--footer starts here--->
+
+
+   </body>
+ </html>
